44b3d0fa76a72874991ec7eb12bddedfd7e4858b
Uzyxen <michal912456@gmail.com>
2024-10-29 20:22:53 +0100

    init

---
+++ konsolowa/konsolowa/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace konsolowa
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            bool playAgain = true;
+            while (playAgain)
+            {
+                Console.Write("Podaj liczbę kostek do rzucenia (3-10): ");
+
+                int numberOfDice;
+                while (!int.TryParse(Console.ReadLine(), out numberOfDice) || numberOfDice < 3 || numberOfDice > 10)
+                {
+                    Console.WriteLine("Nieprawidłowa liczba. Wprowadź liczbę od 3 do 10: ");
+                }
+
+                // Rzut kostkami i zapis wyników
+                List<int> diceResults = RollDice(numberOfDice);
+                DisplayDiceResults(diceResults);
+
+                // Obliczanie punktów
+                int points = CalculatePoints(diceResults);
+                Console.WriteLine($"Twoje punkty: { points }");
+
+                // Zapytanie o powtórzenie gry
+                Console.Write("Czy chcesz zagrać ponownie? (t/n): ");
+                string? response = Console.ReadLine();
+                playAgain = response.ToLower() == "t";
+            }
+        }
+
+        static List<int> RollDice(int count)
+        {
+            Random rand = new Random();
+            List<int> results = new List<int>();
+
+            for (int i = 0; i < count; i++)
+            {
+                results.Add(rand.Next(1, 7));
+            }
+            return results;
+        }
+
+        static void DisplayDiceResults(List<int> results)
+        {
+            for (int i = 0; i < results.Count; i++)
+            {
+                Console.WriteLine($"Kostka {i + 1}: {results[i]}");
+            }
+        }
+
+        static int CalculatePoints(List<int> results)
+        {
+            int points = 0;
+
+            // Grupowanie wyników i liczenie punktów dla wartości powtórzonych
+            var groupedResults = results.GroupBy(x => x)
+                                        .Where(g => g.Count() >= 2); // Tylko liczby, które występują co najmniej 2 razy
+
+            foreach (var group in groupedResults)
+            {
+                points += group.Key * group.Count();
+            }
+
+            return points;
+        }
+    }
+}
---
+++ mobilna/mobilna/mobilna/MainPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace mobilna
+{
+    public partial class MainPage : ContentPage
+    {
+        int gameResult = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Dice0.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+            Dice1.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+            Dice2.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+            Dice3.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+            Dice4.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+        }
+
+        private void DiceRoll(object sender, EventArgs e)
+        {
+            int[] dice = new int[5];
+
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                dice[i] = random.Next(1, 7);
+            }
+
+            Dice0.Source = ImageSource.FromResource("mobilna.Images.k6_" + dice[0] + ".png");
+            Dice1.Source = ImageSource.FromResource("mobilna.Images.k6_" + dice[1] + ".png");
+            Dice2.Source = ImageSource.FromResource("mobilna.Images.k6_" + dice[2] + ".png");
+            Dice3.Source = ImageSource.FromResource("mobilna.Images.k6_" + dice[3] + ".png");
+            Dice4.Source = ImageSource.FromResource("mobilna.Images.k6_" + dice[4] + ".png");
+
+            int rollResult = 0;
+            for (int dots = 1; dots <= 6; dots++)
+            {
+                int count = 0;
+                for (int diceIndex = 0; diceIndex < 5; diceIndex++)
+                {
+                    if (dice[diceIndex] == dots)
+                    {
+                        count++;
+                    }
+                }
+                if (count > 1)
+                {
+                    rollResult += dots * count;
+                }
+            }
+            RollResultLabel.Text = "Wynik tego losowania: " + rollResult;
+            gameResult += rollResult;
+            GameResultLabel.Text = "Wynik gry: " + gameResult;
+        }
+
+        private void ResetGame(object sender, EventArgs e)
+        {
+            Dice0.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+            Dice1.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+            Dice2.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+            Dice3.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+            Dice4.Source = ImageSource.FromResource("mobilna.Images.k6_0.png");
+
+            gameResult = 0;
+            RollResultLabel.Text = "Wynik tego losowania: 0";
+            GameResultLabel.Text = "Wynik gry: " + gameResult;
+        }
+    }
+}
